0de5a8bfbaa0a71cb8978ca006a18cb565e48978
Trinh <trinhtran@bravesoft.com.vn>
2022-09-06 17:30:22 +0700

    oke

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import meter1 from "./../../assets/images/meter1.svg";
 import meter2 from "./../../assets/images/meter2.svg";
 import meter3 from "./../../assets/images/meter3.svg";
 import image_amongus from './../../assets/images/amongus_233_300.png';
+import image_card from './../../assets/images/maxresdefault.jpg'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram, faYoutube, faGithub } from "@fortawesome/free-brands-svg-icons";
 
@@ -12,19 +13,19 @@ const cx = classNames.bind(style)
 function Home(){
     const slide_image = [{
             'image': meter1,
-            'content': "JavaScript"
-        },
-        {
-            'image': meter2,
             'content': "Reactjs"
         },
         {
-            'image': meter3,
+            'image': meter2,
             'content': "Django Rest Framework"
         },
         {
+            'image': meter3,
+            'content': "Dash Plotly"
+        },
+        {
             'image': meter2,
-            'content': "Dash"
+            'content': "Selenium"
         }
     ]
 
@@ -34,6 +35,33 @@ function Home(){
             <p>{slide.content}</p>
         </div>  
     })
+
+    const progress_data = [
+        {'title': "Python",
+         'value': '85'
+        },
+        {'title':'JavaScript',
+         'value' : '80'
+        },
+        {'title':'SQL server',
+         'value' : '65'
+        },
+        {'title':'Microsoft Azure',
+         'value' : '70'
+        },
+        {'title':'Git',
+         'value' : '90'
+        }
+    ]
+    const Progress = progress_data.map((progress, index) => {
+        return <div className={cx('progress_bar')} key={index}>
+            <label>{progress.title}</label>
+            <div>
+                <div style={{width: progress.value+"%"}}>{progress.value + "%"}</div>
+            </div>
+        </div>
+    } )
+
     
     return (
         <div className={cx('wrapper')}>
@@ -72,6 +100,7 @@ function Home(){
                         <label>Skills</label>
                         <p>The page will reload when you make changes. You may also see any lint errors in the console. The page will reload when you make changes. You may also see any lint errors in the console. </p>
                         <div className={cx('skills_slide')}>{slide}</div>
+                        {Progress}
                     </div>
                 </div>
             </div>
@@ -79,7 +108,19 @@ function Home(){
             <div className={cx('inner_project')}>
                 <div className={cx('project')}>
                     <div>
-                        <label>Project</label>
+                        <div>
+                            <label>Project</label>
+                        </div>
+                        <div>
+                            <div>
+                                <img alt="card" src={image_card}></img>
+                                <img alt="card" src={image_card}></img>
+                            </div>
+                            <div >
+                                <img alt="card" src={image_card}></img>
+                                <img alt="card" src={image_card}></img>
+                            </div>
+                        </div>
                     </div>
                     
                 </div>
